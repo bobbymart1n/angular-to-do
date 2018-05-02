d718dc28c4d335735057b231ba8441693d276302
Bobby Martin and Nick Kardish <bobby@>
2018-05-02 14:57:24 -0700

    add pipes

---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../models/task.model';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent implements OnInit {
+  @Input() childTaskList: Task[];
+  @Output() clickSender = new EventEmitter();
+  editButtonClicked(taskToEdit: Task) {
+    this.clickSender.emit(taskToEdit);
+  }
+  filterByCompleteness: string = "incompleteTasks";
+  priorityColor(currentTask){
+    if (currentTask.priority === 3){
+      return "bg-danger";
+    } else if (currentTask.priority === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
+
+  onChange(optionFromMenu) {
+    this.filterByCompleteness = optionFromMenu;
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+  toggleDone(clickedTask: Task, setCompleteness: boolean) {
+    clickedTask.done = setCompleteness;
+  }
+}
